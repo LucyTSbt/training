be240cc1ab829659f78af4952515d3713fb99e61
Lucy T <luci_s@rambler.ru>
2016-08-20 00:58:41 +0300

    Added implementation of simple server

---
+++ echo-parent/echo-common/src/main/java/ru/sbt/echo/Constant.java
@@ -0,0 +1,12 @@
+package ru.sbt.echo;
+
+/**
+ * Created by ZZZ on 20.08.2016.
+ */
+public abstract class Constant {
+
+    public static final int DEFAULT_PORT = 9999;
+    public static final String TEST_REQUEST = "Test request";
+    public static final String TEST_RESPONSE = "Test response";
+    public static final String UNKNOWN_REQUEST = "Unknown request";
+}
--- echo-parent/echo-server/src/main/java/ru/sbt/echo/server/EchoServer.java
+++ echo-parent/echo-server/src/main/java/ru/sbt/echo/server/EchoServer.java
@@ -1,11 +1,49 @@
 package ru.sbt.echo.server;
 
-public class EchoServer {
+import ru.sbt.echo.Constant;
 
-    private static final int DEFAULT_PORT = 9999;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
 
     public static void main(String[] args){
 
+        try {
+            // создать сокет
+            ServerSocket serverSocket = new ServerSocket(Constant.DEFAULT_PORT);
+            // ожидание обращения
+            try (Socket socket = serverSocket.accept()) {
+                // получить потоки ввода/вывода
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter out = new PrintWriter(outputStream, true);
+                out.println("Echo Server: Hello! Enter exit to quit.");
+                // считать данные из строки
+                Scanner in = new Scanner(inputStream);
+                Boolean isExit = false;
+                while (!isExit && in.hasNextLine()) {
+                    String line = in.nextLine().trim();
+                    if (line.compareToIgnoreCase(Constant.TEST_REQUEST) == 0) {
+                        out.println("Echo Server: " + Constant.TEST_RESPONSE);
+                    } else{
+                        if(line.equals("exit")){
+                            isExit = true;
+                        } else {
+                            out.println("Echo Server: " + Constant.UNKNOWN_REQUEST);
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
