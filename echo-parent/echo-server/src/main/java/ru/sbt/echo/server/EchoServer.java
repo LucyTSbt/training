d2d3fba0421921ce21d814349d51f2eba46ad3b5
a1vanov <a1vanov@yandex.ru>
2016-10-03 16:32:29 +0300

    Notes added.

--- echo-parent/echo-server/src/main/java/ru/sbt/echo/server/EchoServer.java
+++ echo-parent/echo-server/src/main/java/ru/sbt/echo/server/EchoServer.java
@@ -1,10 +1,11 @@
 package ru.sbt.echo.server;
 
+
 public class EchoServer {
 
     public static void main(String[] args){
 
-        Server server = new Server();
+        Server server = new Server(); // запуск нового потока. С какой целью?
         server.serverStart();
 
     }
--- echo-parent/echo-server/src/main/java/ru/sbt/echo/server/Server.java
+++ echo-parent/echo-server/src/main/java/ru/sbt/echo/server/Server.java
@@ -17,25 +17,29 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Сервер
+ * Нарушение Single Responsibility Principle: https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%B8%D0%BD%D1%86%D0%B8%D0%BF_%D0%B5%D0%B4%D0%B8%D0%BD%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B9_%D0%BE%D0%B1%D1%8F%D0%B7%D0%B0%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8
+ * Этот класс сейчас спроектирован для использования в двух потоках и в каждом из них он выполняет свою функцию.
+ * При этом один поток он запускает сам, а другой долже вызывать его извне: очень запутанная логика.
+ * Нужно устранить нарушение SRP
  */
 public class Server extends Thread {
 
-    private final static Logger LOGGER  = LoggerFactory.getLogger(Server.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     private ServerSocket serverSocket;
     private ServerSocket serverStopSocket;
     private ExecutorService executorService;
     private Boolean stopped;
 
-    public Server(){
+    public Server() {
         // создать сокет
-        try{
+        try {
             serverSocket = new ServerSocket(Constant.DEFAULT_PORT);
             serverStopSocket = new ServerSocket(Constant.STOP_PORT);
             executorService = Executors.newCachedThreadPool();
             stopped = false;
             //setDaemon(true);
-            start();
+            start(); // Почему так делать не нужно и другие полезные советы:  https://www.ibm.com/developerworks/ru/library/j-jtp0618/
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
             LOGGER.debug(e.getMessage(), e);
@@ -43,8 +47,8 @@ public class Server extends Thread {
     }
 
     @Override
-    public void run(){
-        try{
+    public void run() {
+        try {
             System.out.println("Echo Server start");
             LOGGER.info("Echo Server start on port {}", Constant.DEFAULT_PORT);
             // ожидание обращения
@@ -62,17 +66,16 @@ public class Server extends Thread {
         }
     }
 
-    public void serverStart(){
+    public void serverStart() {
         LOGGER.info("Echo Server listens on port {}", Constant.STOP_PORT);
 
         try (Socket socket = serverStopSocket.accept()) {
-            BufferedReader in  = new BufferedReader(new
-                    InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             String input;
 
             while ((input = in.readLine()) != null) {
-                if (input.equalsIgnoreCase("stop")){
+                if (input.equalsIgnoreCase("stop")) {
                     out.println(input);
                     LOGGER.info("Echo Server received a stop command");
                     break;
@@ -88,7 +91,7 @@ public class Server extends Thread {
         }
     }
 
-    private void serverStop(){
+    private void serverStop() {
         try {
             LOGGER.info("Echo Server is stopping");
             stopped = true;
